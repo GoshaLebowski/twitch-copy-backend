1f1aa5033f5043f97b08568c1ad0e51618194951
GoshaLebowski <sirinkinegor@gmail.com>
2025-01-21 16:09:13 +0500

    Деактивацию аккаунта
    1. Отправка на почту деактивации аккаунта
    
    2. Валидация кода для деактивации аккаунта
    
    3. Функция деактивации аккаунта которая объединяет отправку на почту и валидацию кода для деактивации

--- src/modules/libs/mail/mail.service.ts
+++ src/modules/libs/mail/mail.service.ts
@@ -1,13 +1,12 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { render } from '@react-email/components';
+import { render } from '@react-email/components'
 
-
-
-import { PasswordRecoveryTemplate } from '@/src/modules/libs/mail/templates/password-recovery.template';
-import { VerificationTemplate } from '@/src/modules/libs/mail/templates/verification.template'
-import type { SessionMetadata } from '@/src/shared/types/session-metadata.types'
+import { DeactivateTemplate } from '@/src/modules/libs/mail/templates/deactivate.template'
+import { PasswordRecoveryTemplate } from '@/src/modules/libs/mail/templates/password-recovery.template'
+import { VerificationTemplate } from '@/src/modules/libs/mail/templates/verification.template';
+import type { SessionMetadata } from '@/src/shared/types/session-metadata.types';
 
 
 
@@ -38,6 +37,16 @@ export class MailService {
         )
 
         return this.sendMail(email, 'Сброс пароля', html)
+    }
+
+    public async sendDeactivateToken(
+        email: string,
+        token: string,
+        metadata: SessionMetadata
+    ) {
+        const html = await render(DeactivateTemplate({ token, metadata }))
+
+        return this.sendMail(email, 'Деактивация аккаунта', html)
     }
 
     private sendMail(email: string, subject: string, html: string) {
