4936998059deb380c96486cda28f534e5fd8ac48
GoshaLebowski <sirinkinegor@gmail.com>
2025-02-13 18:11:08 +0500

    Cron операция
    1. Получение списка пользователей
    
    2. Отправка сообщений на почту
    
    3. Удаление аккаунта

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -7,16 +7,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 import { getGraphQLConfig } from '@/src/core/config/graphql.config';
 import { AccountModule } from '@/src/modules/auth/account/account.module';
+import { DeactivateModule } from '@/src/modules/auth/deactivate/deactivate.module';
 import { PasswordRecoveryModule } from '@/src/modules/auth/password-recovery/password-recovery.module';
 import { SessionModule } from '@/src/modules/auth/session/session.module';
+import { TotpModule } from '@/src/modules/auth/totp/totp.module';
 import { VerificationModule } from '@/src/modules/auth/verification/verification.module';
+import { CronModule } from '@/src/modules/cron/cron.module';
 import { MailModule } from '@/src/modules/libs/mail/mail.module';
-import { IS_DEV_ENV } from '@/src/shared/utils/is-dev.util'
+import { IS_DEV_ENV } from '@/src/shared/utils/is-dev.util';
 
-import { PrismaModule } from './prisma/prisma.module'
-import { RedisModule } from './redis/redis.module'
-import { TotpModule } from '@/src/modules/auth/totp/totp.module'
-import { DeactivateModule } from '@/src/modules/auth/deactivate/deactivate.module'
+
+
+import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
 
 
 
@@ -40,9 +43,10 @@ import { DeactivateModule } from '@/src/modules/auth/deactivate/deactivate.modul
         SessionModule,
         VerificationModule,
         MailModule,
+        CronModule,
         PasswordRecoveryModule,
         TotpModule,
-        DeactivateModule,
+        DeactivateModule
     ]
 })
 export class CoreModule {}
--- src/modules/libs/mail/mail.service.ts
+++ src/modules/libs/mail/mail.service.ts
@@ -1,12 +1,15 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { render } from '@react-email/components'
+import { render } from '@react-email/components';
 
-import { DeactivateTemplate } from '@/src/modules/libs/mail/templates/deactivate.template'
+
+
+import { AccountDeletionTemplate } from '@/src/modules/libs/mail/templates/account-deletion.template';
+import { DeactivateTemplate } from '@/src/modules/libs/mail/templates/deactivate.template';
 import { PasswordRecoveryTemplate } from '@/src/modules/libs/mail/templates/password-recovery.template'
-import { VerificationTemplate } from '@/src/modules/libs/mail/templates/verification.template';
-import type { SessionMetadata } from '@/src/shared/types/session-metadata.types';
+import { VerificationTemplate } from '@/src/modules/libs/mail/templates/verification.template'
+import type { SessionMetadata } from '@/src/shared/types/session-metadata.types'
 
 
 
@@ -47,6 +50,13 @@ export class MailService {
         const html = await render(DeactivateTemplate({ token, metadata }))
 
         return this.sendMail(email, 'Деактивация аккаунта', html)
+    }
+
+    public async sendAccountDeletion(email: string) {
+        const domain = this.configService.getOrThrow<string>('ALLOWED_ORIGIN')
+        const html = await render(AccountDeletionTemplate({ domain }))
+
+        return this.sendMail(email, 'Аккаунт удалён', html)
     }
 
     private sendMail(email: string, subject: string, html: string) {
