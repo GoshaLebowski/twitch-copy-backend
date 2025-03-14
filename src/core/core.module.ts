e2b318d1408bbb1906c82f90707a5b4c1b8f9658
GoshaLebowski <sirinkinegor@gmail.com>
2025-03-14 15:11:04 +0500

    Настройка LiveKit
    1. Установка пакета livekit-server-sdk@1.2.7
    
    2. Настройка модуля livekit.module.ts и его настройка в core.module.ts
    
    3. Создание proxy в livekit.service.ts
    
    4. Создание livekit.config.ts

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 
 import { getGraphQLConfig } from '@/src/core/config/graphql.config';
+import { getLiveKitConfig } from '@/src/core/config/livekit.config';
 import { AccountModule } from '@/src/modules/auth/account/account.module';
 import { DeactivateModule } from '@/src/modules/auth/deactivate/deactivate.module';
 import { PasswordRecoveryModule } from '@/src/modules/auth/password-recovery/password-recovery.module';
@@ -14,6 +15,7 @@ import { SessionModule } from '@/src/modules/auth/session/session.module';
 import { TotpModule } from '@/src/modules/auth/totp/totp.module';
 import { VerificationModule } from '@/src/modules/auth/verification/verification.module';
 import { CronModule } from '@/src/modules/cron/cron.module';
+import { LivekitModule } from '@/src/modules/libs/livekit/livekit.module';
 import { MailModule } from '@/src/modules/libs/mail/mail.module';
 import { StorageModule } from '@/src/modules/libs/storage/storage.module';
 import { StreamModule } from '@/src/modules/stream/stream.module';
@@ -38,6 +40,11 @@ import { RedisModule } from './redis/redis.module';
             driver: ApolloDriver,
             imports: [ConfigModule],
             useFactory: getGraphQLConfig,
+            inject: [ConfigService]
+        }),
+        LivekitModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: getLiveKitConfig,
             inject: [ConfigService]
         }),
         PrismaModule,
