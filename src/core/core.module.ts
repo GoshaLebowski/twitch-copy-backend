0827bc8842d34af835c5020a59857af12c4eb4db
GoshaLebowski <sirinkinegor@gmail.com>
2025-03-20 20:54:38 +0500

    Сущность для канала
    1. Создан channel.module.ts и подписан в core.module.ts
    
    2. Создан channel.service.ts и написана логика вывод рекомендаций, вывод по username и количество подписчиков
    
    4. Создан follow.resolver.ts и прописаны контрольные точки для вывода рекомендаций, вывода по username и количеству подписчиков на канале

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -15,6 +15,7 @@ import { SessionModule } from '@/src/modules/auth/session/session.module';
 import { TotpModule } from '@/src/modules/auth/totp/totp.module';
 import { VerificationModule } from '@/src/modules/auth/verification/verification.module';
 import { CategoryModule } from '@/src/modules/category/category.module';
+import { ChannelModule } from '@/src/modules/channel/channel.module';
 import { ChatModule } from '@/src/modules/chat/chat.module';
 import { CronModule } from '@/src/modules/cron/cron.module';
 import { FollowModule } from '@/src/modules/follow/follow.module';
@@ -69,7 +70,8 @@ import { RedisModule } from './redis/redis.module';
         WebhookModule,
         CategoryModule,
         ChatModule,
-        FollowModule
+        FollowModule,
+        ChannelModule
     ]
 })
 export class CoreModule {}
